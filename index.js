60069aca028526d765a485ba2dabd18e173cec77
david <davmwa90@gmail.com>
2022-12-07 08:39:23 +0300

    Completed a Fetch function lab

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
+// Using fetch function 
 function fetchBooks() {
-  // To pass the tests, don't forget to return your fetch!
-  
+  return fetch('https://anapioficeandfire.com/api/books')
+  .then(res => res.json())
+  .then(json => renderBooks(json))
 }
-
+// Calling a function within another function 
 function renderBooks(books) {
   const main = document.querySelector('main');
   books.forEach(book => {
